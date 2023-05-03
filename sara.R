84702cb60d4cd382d3fd98d010693d74af16e728
Sara Schmitt <sjschm23@g.holycross.edu>
2023-05-02 21:37:30 -0400

    create double deck of cards and deal function

--- sara.R
+++ sara.R
@@ -1,6 +1,24 @@
-a = 1 
-b = 2 
-c = 3
-f = 5
-frfrfef = 3424
-fdslfksdl = 4324324
+library(purrr)
+library(dplyr)
+
+## 2 Decks of Cards (2-3 person game)
+
+suit.names = c('spades', 'clubs', 'hearts', 'diamonds')
+suits = unlist(map(suit.names, rep, 26))
+
+face.names = c('king', 'queen', 'jack', 'ten', 'nine', 'eight', 'seven', 'six', 'five', 'four', 'three', 'two', 'ace')
+faces = rep(face.names, 8)
+
+deck = data.frame(faces,suits)
+joker = c('joker')
+faces = rep(joker,4)
+joker.suits = c('NA')
+suits = rep(joker.suits,4)
+total.jokers = data.frame(faces,suits)
+deck = rbind(deck,total.jokers)
+
+#Deal Function (num cards dealt as parameter)
+
+deal = function(n) {
+  sample_n(deck,n,replace=F)
+}
