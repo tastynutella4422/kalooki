5901cb5fc54d353f8b48d3f158967a1b17d2f884
Elias Berhane <eliasberhane@gmail.com>
2023-05-04 13:47:13 -0400

    new shuffling updates

--- elias.R
+++ elias.R
@@ -22,47 +22,72 @@ deck = rbind(deck,total.jokers)
 
 #Deal Function (num cards dealt as parameter)
 deal = function(n, deck) {
-  dealt.cards = deck[sample(nrow(deck), n),]
-  remaining.cards = anti_join(deck,dealt.cards, by="order")
-  return(list(v1=dealt.cards, v2=remaining.cards))
+  #Create empty data frames for player1 and player2
+  player1_cards = data.frame()
+  player2_cards = data.frame()
+  
+  # Set the number of cards to deal to each player
+  num_cards = n
+  
+  # Simulate dealing the cards
+  for (i in 1:(2 * num_cards)) {
+    if (i %% 2 == 1) { # Odd row numbers go to player1
+      player1_cards = rbind(player1_cards, deck[i,])
+    } else { # Even row numbers go to player2
+      player2_cards = rbind(player2_cards, deck[i,])
+    }
+  }
+  
+  # Remove the dealt cards from the original deck
+  remaining.cards= deck[-(1:(2 * num_cards)), ]
+  
+  return(list(v1=player1_cards, v2=player2_cards, v3=remaining.cards))
 }
 
 #Beginning of each round setup - dealing & discard pile
 setup = function(n, deck) {
   num.cards = n
-  p1 = deal(num.cards, deck)
-  p1.hand = p1$v1
-  remaining = p1$v2
-  p2 = deal(num.cards, remaining)
-  p2.hand = p2$v1
-  remaining = p2$v2
-  top.card = remaining[sample(nrow(remaining), 1),]
-  stock.pile = anti_join(remaining,top.card, by="order")
-  print(top.card)
-  return(list(v1=p1.hand, v2=p2.hand, v3=top.card, v4=stock.pile))
+  
+  #shuffle the deck first
+  shuffled.deck = deck[sample(nrow(deck), nrow(deck), replace = FALSE), ]
+
+  resulting.hands = deal(num.cards, shuffled.deck)
+  p1.hand = resulting.hands$v1
+  p2.hand = resulting.hands$v1
+  remaining = resulting.hands$v3
+  
+  top.discard.card = remaining[1,] #assigns the top value of the remaining cards
+  
+  stock.pile = remaining[-1, ]
+  print(top.discard.card)
+  return(list(v1=p1.hand, v2=p2.hand, v3=top.discard.card, v4=stock.pile))
 }
+
 
 #Round 1: Three 3s
 
 init.cards = setup(12, deck)
 p1.hand = init.cards$v1
 p2.hand = init.cards$v2
-top.card = init.cards$v3
+top.discard = init.cards$v3
 stock.pile = init.cards$v4
-
-
-choose = runif(1)
-whogoesfirst = p1
-if(choose <.5) {
-  print("player 1 chooses first")
-  whogoesfirst = 'p1'
+choose = runif(1) #decides which player goes first
+if (choose <= .5) {
+  cat("Player 1 goes first")
+  first.player = p1.hand
+  second.player = p2.hand
 } else {
-  print("player 2 chooses first")
-  whogoesfirst = 'p2'
+  cat("Player 2 goes first")
+  first.player = p2.hand
+  second.player = p1.hand
 }
 discard.stock = runif(1)
-if(discard.stock < .5) {
-  cat("Player", whogoesfirst, "chooses from the stock pile")
+if (discard.stock <= .5) {
+  first.player = rbind(first.player,top.discard)
+  top.discard = NULL
 } else {
-  cat("Player", whogoesfirst, "chooses from the discard pile")
+  top.stock = stock.pile[1, ]
+  first.player = rbind(first.player,top.stock)
+  stock.pile = stock.pile[-1,]
 }
+
--- main.R
+++ main.R
@@ -1,12 +1,6 @@
-#this is where the main project will go 
-card = 10
-s = 20 
-f = 30
-
-a = 6 
-b = 4 
-n = 123
-fdj = 2948
-
-frfrfef = 3424
-fdslfksdl = 4324324
+suits <- c("Clubs", "Diamonds", "Hearts", "Spades")
+ranks <- c(2:10, "Jack", "Queen", "King", "Ace")
+deck <- expand.grid(Suit = suits, Rank = ranks)
+deck <- rbind(deck, deck) # Add a second copy of the deck
+jokers <- data.frame(Suit = c("Joker", "Joker"), Rank = c("Joker", "Joker"))
+deck <- rbind(deck, jokers) # Add the jokers
