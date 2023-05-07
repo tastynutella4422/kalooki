7dc21498c0e4e708a1b47b5a6811ba1127afc2b2
Sara Schmitt <sjschm23@g.holycross.edu>
2023-05-07 15:51:54 -0400

    made code for one round of 3 3s into a function

--- sara.R
+++ sara.R
@@ -5,6 +5,7 @@ library(dplyr)
 
 laid.down.cards = data.frame()
 
+
 suit.names = c('spades', 'clubs', 'hearts', 'diamonds')
 suits = unlist(map(suit.names, rep, 26))
 
@@ -65,7 +66,7 @@ setup = function(n, deck) {
   return(list(v1=p1.hand, v2=p2.hand, v3=top.discard.card, v4=stock.pile))
 }
 
-#determine which card in hand is of highest value (to be used when discarding during each player's turn)
+#function to determine which card in hand is of highest value (to be used when discarding during each player's turn)
 highest.val.card = function(hand) {
   face.vals <- c("two" = 2, "three" = 3, "four" = 4, "five" = 5, "six" = 6, "seven" = 7,
                    "eight" = 8, "nine" = 9, "ten" = 10, "jack" = 11, "queen" = 12, "king" = 13)
@@ -75,80 +76,113 @@ highest.val.card = function(hand) {
   return(hand[which.max(hand$value), ])
 }
 
+#function to determine who goes first 
+player.order = function(p1.hand,p2.hand) {
+  choose = runif(1) #decides which player goes first
+  if (choose <= .5) {
+    cat("Player 1 goes first")
+    first.player = p1.hand
+    second.player = p2.hand
+  } else {
+    cat("Player 2 goes first")
+    first.player = p2.hand
+    second.player = p1.hand
+  }
+  return(list(v1=first.player, v2=second.player))
+}
+
+#function to determine if player will pick up from discard pile or stock pile
+discard.or.stock = function(player) {
+  discard.stock = runif(1)
+  if (discard.stock <= .5) {
+    player = rbind(player,top.discard)
+    top.discard = NULL
+  } else {
+    top.stock = stock.pile[1, ]
+    player = rbind(player,top.stock)
+    stock.pile = stock.pile[-1,]
+  }
+  return(list(v1=player, v2=top.discard, v3=stock.pile))
+}
+
+#function that checks for 3s and partial 3s, lays down any 3s, and chooses card to discard (excluding sets of 2 since they have higher chance of becoming a 3)
+three.threes.gameplay = function(player,total.threes,total.laid.down.cards) {
+  player.faces = player$faces #vector of all the face values in player's hand (ie. king, ace, ace, one, etc.)
+  face.counts = rep(0,length(player.faces)) #vector that has count of each face value in a player's hand (index 1 = king, index 13 = ace)
+  set.of.three = c()
+  set.of.two = c()
+  # loop through player's faces vector and count how many faces in the hand
+  for (i in 1:length(player.faces)) {
+    for (j in 1:length(player.faces)) {
+      if (face.names[i] == player.faces[j]) {
+        face.counts[i] = face.counts[i] + 1
+      }
+    }
+  }
+  for (k in 1:length(face.counts)) {
+    if (face.counts[k] == 2) {
+      set.of.two = append(set.of.two,face.names[k]) # vector of sets of 2 (close to a 3)
+    }
+    if (face.counts[k] >= 3) {
+      set.of.three = append(set.of.three,face.names[k]) # vector of face vals that occur 3+ times
+    } 
+  }
+  
+  if (length(set.of.three > 0)) { #check to see if any cards can be laid down
+    for (i in 1:length(set.of.three)) {
+      dups = set.of.three[i] 
+      laid.down.cards = filter(player, faces %in% dups) # print full card names in p1's hand that were marked as a 3
+      total.laid.down.cards = rbind(total.laid.down.cards,laid.down.cards)
+    }
+    cat("Player laid down: \n")
+    print(laid.down.cards)
+    total.threes = total.threes + length(set.of.three)
+    remaining.hand = anti_join(player,laid.down.cards, by="order")
+  } else {
+    remaining.hand = player
+  }
+  
+  #convert set.of.two to dataframe so that it can be removed from consideration when discarding cards
+  for (i in 1:length(set.of.two)) {
+    partial = set.of.two[i]
+    partial.sets = filter(remaining.hand, faces %in% partial)
+  }
+  
+  #from the remaining hand, discard the highest value card (elias's function)
+  to.discard = anti_join(remaining.hand,partial.sets, by="order")
+  player.discard = highest.val.card(to.discard)
+  end.of.turn.hand = anti_join(player,player.discard, by="order")
+  top.discard = player.discard #reassign top of the discard pile
+ 
+  return(list(v1=end.of.turn.hand, v2=total.threes, v3=top.discard, v4=total.laid.down.cards)) 
+}
 
 #Round 1: Three 3s
+
+p1.total.threes = 0
+p2.total.threes = 0
+total.laid.down.cards = data.frame()
 
 init.cards = setup(12, deck)
 p1.hand = init.cards$v1
 p2.hand = init.cards$v2
 top.discard = init.cards$v3
 stock.pile = init.cards$v4
-choose = runif(1) #decides which player goes first
-if (choose <= .5) {
-  cat("Player 1 goes first")
-  first.player = p1.hand
-  second.player = p2.hand
-} else {
-  cat("Player 2 goes first")
-  first.player = p2.hand
-  second.player = p1.hand
-}
-discard.stock = runif(1)
-if (discard.stock <= .5) {
-  first.player = rbind(first.player,top.discard)
-  top.discard = NULL
-} else {
-  top.stock = stock.pile[1, ]
-  first.player = rbind(first.player,top.stock)
-  stock.pile = stock.pile[-1,]
-}
 
-#check for duplicates (mark 3s and sets getting close to 3)
-#using face.names from above to keep track of the counts
-total.threes = 0
-p1.faces = first.player$faces #vector of all the face values in p1's hand (ie. king, ace, ace, one, etc.)
-face.counts = rep(0,13) #vector that has count of each face value in a player's hand (index 1 = king, index 13 = ace)
-set.of.three = c()
-set.of.two = c()
-# loop through p1's faces vector and count how many faces in the hand
-for (i in 1:length(p1.faces)) {
-  for (j in 1:length(p1.faces)) {
-    if (face.names[i] == p1.faces[j]) {
-      face.counts[i] = face.counts[i] + 1
-    }
-  }
-}
-for (k in 1:length(face.counts)) {
-  if (face.counts[k] == 2) {
-    set.of.two = append(set.of.two,face.names[k]) # vector of sets of 2 (close to a 3)
-  }
-  if (face.counts[k] >= 3) {
-    set.of.three = append(set.of.three,face.names[k]) # vector of face vals that occur 3+ times
-  } 
-}
+players = player.order(p1.hand,p2.hand)
+first.player = players$v1
+second.player = players$v2
 
-if (length(set.of.three > 0)) { #check to see if any cards can be laid down
-  for (i in 1:length(set.of.three)) {
-    dups = set.of.three[i] 
-    laid.down.cards = filter(first.player, faces %in% dups) # print full card names in p1's hand that were marked as a 3
-  }
-  cat("Player 1 laid down: \n")
-  print(laid.down.cards)
-  total.threes = total.threes + length(set.of.three)
-  remaining.hand = anti_join(first.player,laid.down.cards, by="order")
-} else {
-  remaining.hand = first.player
-}
+picked.up.card = discard.or.stock(first.player)
+first.player = picked.up.card$v1
+top.discard = picked.up.card$v2
+stock.pile = picked.up.card$v3
 
-#convert set.of.two to dataframe so that it can be removed from consideration when discarding cards
-for (i in 1:length(set.of.two)) {
-  partial = set.of.two[i]
-  partial.sets = filter(remaining.hand, faces %in% partial)
-}
+p1.gameplay = three.threes.gameplay(p1.hand,p1.total.threes, total.laid.down.cards)
+p1.hand = p1.gameplay$v1
+p1.total.threes = p1.gameplay$v2
+top.discard = p1.gameplay$v3
+total.laid.down.cards = p1.gameplay$v4
 
-#from the remaining hand, discard the highest value card (elias's function)
-to.discard = anti_join(remaining.hand,partial.sets, by="order")
-player.discard = highest.val.card(to.discard)
-end.of.turn.hand = anti_join(first.player,player.discard, by="order")
-top.discard = player.discard #reassign top of the discard pile
+
 
